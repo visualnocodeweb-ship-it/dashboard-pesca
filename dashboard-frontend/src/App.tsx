b8d647d1502b7108c6d28cd729cd63b91c498663
Ema <faunapescaycaza@gmail.com>
2025-12-12 10:15:51 -0300

    feat: Introduce minimal TestApi component for debugging mobile

--- dashboard-frontend/src/App.tsx
+++ dashboard-frontend/src/App.tsx
@@ -1,38 +1,13 @@
-import { Routes, Route, NavLink } from 'react-router-dom';
-
-import './App.css';
-import CategoriaPescaDashboard from './components/CategoriaPescaDashboard';
-import LatestRecordsDashboard from './components/LatestRecordsDashboard';
-import PermisosDashboard from './components/PermisosDashboard';
-import RecaudacionDashboard from './components/RecaudacionDashboard';
-import RegionesDashboard from './components/RegionesDashboard';
-import ReportesDashboard from './components/ReportesDashboard';
+import TestApi from './components/TestApi';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/Guardafauna - 1.png" alt="Fauna NQN Logo" className="header-logo" style={{ height: '50px', marginRight: '10px' }} />
-        <h1>Temporada 2025-2026</h1>
-        <nav className="main-nav">
-          <NavLink to="/permisos" className="nav-button">Permisos</NavLink>
-          <NavLink to="/recaudacion" className="nav-button">Recaudación</NavLink>
-          <NavLink to="/categorias" className="nav-button">Categorías</NavLink>
-          <NavLink to="/regiones" className="nav-button">Regiones</NavLink>
-          <NavLink to="/ultimos-registros" className="nav-button">Últimos Registros</NavLink>
-          <NavLink to="/reportes" className="nav-button">Reportes</NavLink>
-        </nav>
+        <h1>Prueba de API Simplificada</h1>
       </header>
       <main>
-        <Routes>
-          <Route path="/permisos" element={<PermisosDashboard />} />
-          <Route path="/recaudacion" element={<RecaudacionDashboard />} />
-          <Route path="/categorias" element={<CategoriaPescaDashboard />} />
-          <Route path="/regiones" element={<RegionesDashboard />} />
-          <Route path="/ultimos-registros" element={<LatestRecordsDashboard />} />
-          <Route path="/reportes" element={<ReportesDashboard />} />
-          <Route path="/" element={<PermisosDashboard />} />
-        </Routes>
+        <TestApi />
       </main>
     </div>
   );
